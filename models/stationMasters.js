89a4a706b4d6982e5057ddb3bab196a2cede50f6
unknown <adithkrishnaor@gmail.com>
2024-10-19 23:10:58 +0530

    edited schema

--- models/stationMasters.js
+++ models/stationMasters.js
@@ -1,37 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const stationMasterSchema = new mongoose.Schema({
-    "fullName": { 
-        type: String, 
-        required: true 
-    },
-  "email": { 
-    type: String, 
-    required: true, 
-    unique: true 
-},
-"password": { 
-    type: String, 
-    required: true 
-},
-  
-  "phoneNumber": { 
-    type: Number, 
-    required: true },
-  "address": { 
-    type: String, 
-    required: true },
-  "companyName": { 
-    type: String, 
-    required: true },
-  "role": { 
-    type: String, 
-    default: 'stationMaster' },
-  "createdAt": { type: Date, 
-    default: Date.now },
-  "updatedAt": { type: Date, 
-    default: Date.now }
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  companyName: {
+    type: String,
+    required: true,
+  },
 });
 
-var stationMasterModel = mongoose.model("StationMasters",stationMasterSchema)
-module.exports = stationMasterModel
+var stationMasterModel = mongoose.model("StationMasters", stationMasterSchema);
+module.exports = stationMasterModel;
